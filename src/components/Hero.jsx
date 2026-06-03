e4eff2144719a278ec801547dc1d771fdc614d92
Shubhu-1010 <shubhangibhandare1010@gmail.com>
2026-06-03 16:33:45 +0530

    Fixed Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,10 +12,6 @@ function Hero() {
       </p>
 
       <div className="people">
-        <img src="https://randomuser.me/api/portraits/men/1.jpg" />
-        <img src="https://randomuser.me/api/portraits/men/2.jpg" />
-        <img src="https://randomuser.me/api/portraits/men/3.jpg" />
-        <img src="https://randomuser.me/api/portraits/men/4.jpg" />
       </div>
     </section>
   );
